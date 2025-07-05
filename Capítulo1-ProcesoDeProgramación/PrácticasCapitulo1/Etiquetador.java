67ddc55570bf7e9cbcfb8e55e59197488d881d87
carrada <carrada@ciencias.unam.mx>
2025-07-04 18:45:32 -0600

    practicas Capitulo1

--- Capítulo1-ProcesoDeProgramación/PrácticasCapitulo1/Etiquetador.java
+++ Capítulo1-ProcesoDeProgramación/PrácticasCapitulo1/Etiquetador.java
@@ -1,3 +1,11 @@
 public class Etiquetador {
-    
+    public static void main(String[] args) {
+        System.out.println("Mr. James Bond");
+        System.out.println("Agente 007");
+        System.out.println("Av. Insurgentes Sur 4411");
+        System.out.println("Resiencial Insurgentes Sur");
+        System.out.println("Edificio \"Rosales\", Int. #15");
+        System.out.println("Col. La Joya, C.P. 14430");
+        System.out.println("México D.F.");
+    }
 }
