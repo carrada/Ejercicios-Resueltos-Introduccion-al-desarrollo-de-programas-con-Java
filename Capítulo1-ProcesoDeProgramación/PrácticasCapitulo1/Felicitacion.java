40b2504e8c09e293de6dbf9e041577784f16c3a6
carrada <carrada@ciencias.unam.mx>
2025-07-04 18:39:47 -0600

    implementación de practicas del capitulo 1

---
+++ Capítulo1-ProcesoDeProgramación/PrácticasCapitulo1/Felicitacion.java
@@ -0,0 +1,12 @@
+public class Felicitacion {
+    public static void main(String[] args) {
+        System.out.println("!!Muchas Felicidades Amparo!!");
+        System.out.println(" ");
+        System.out.println("En la familia estamos que no cabemos de gusto y orgullo porque estas");
+        System.out.println("aprendiendo a programar en Java con orientación a objetos.");
+        System.out.println(" ");
+        System.out.println("De verdad \"!! Muchas Felicidades !!\"");
+        System.out.println(" ");
+        System.out.println("Un abrazo :)");
+    }
+}
